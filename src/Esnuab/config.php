4b799c189a972a58817aad3dbd00bfaada36ec65
AlejandroHerr <alejandrohnc88@gmail.com>
2013-11-25 20:55:46 +0100

    Update config.php

--- src/Esnuab/config.php
+++ src/Esnuab/config.php
@@ -3,23 +3,14 @@
 //define('ROOT',dirname(dirname(__DIR__)));
 
 $loader = require ROOT . "/vendor/autoload.php";
-
+require_once 'db.php';
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver' => 'pdo_mysql',
-        'host' => '',
-        'dbname' => '',
-        'user' => '',
-        'password' => '',
-        'charset' => 'utf8'
-    )
-));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), $configDB);
 
 $app['socio_manager'] = $app->share(function($app) {
     return new \Esnuab\Libro\Model\SocioManager($app['db']);
