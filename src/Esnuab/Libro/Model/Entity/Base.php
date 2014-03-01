f38430e7864296ca46ec537442a8b9d63b304b2d
AlejandroHerr <alejandrohnc88@gmail.com>
2014-03-01 03:55:36 +0100

    Formated under PSR-2 standard

--- src/Esnuab/Libro/Model/Entity/Base.php
+++ src/Esnuab/Libro/Model/Entity/Base.php
@@ -2,54 +2,56 @@
 
 namespace Esnuab\Libro\Model\Entity;
 
-abstract class Base{
-  function __construct(array $data=array()){
-	  foreach ($data as $key => $value) {
-		$this->__set($key,$value);
-	  }
-	}
+abstract class Base
+{
+    function __construct(array $data=array())
+    {
+        foreach ($data as $key => $value) {
+            $this->__set($key,$value);
+        }
+    }
 
-	function __get($name) {
-	  $method = "get".ucwords($name);
-	  if(method_exists($this,$method)):
-		return $this->$method();
-	  elseif (property_exists($this, $name)):
-		return $this->$name;
-	  endif;
-	}
+    function __get($name)
+    {
+        $method = "get".ucwords($name);
+        if(method_exists($this,$method)):
+            return $this->$method();
+        elseif (property_exists($this, $name)):
+            return $this->$name;
+        endif;
+    }
 
-	function __set($name, $value) {
-	  $method = "set".ucwords($name);
-	  if(method_exists($this, $method)):
-		return $this->$method($value);
-	  elseif (property_exists($this, $name)):
-		$this->$name = $value;
-	  endif;
-	}
- function __toString(){
-	  ob_start();
-	  var_dump($this);
-	  return ob_get_clean();
-	}
+    function __set($name, $value)
+    {
+        $method = "set".ucwords($name);
+        if(method_exists($this, $method)):
+            return $this->$method($value);
+        elseif (property_exists($this, $name)):
+            $this->$name = $value;
+        endif;
+    }
+     function __toString()
+     {
+        ob_start();
+        var_dump($this);
+        return ob_get_clean();
+    }
+    function toArray()
+    {
+        $array = @array_filter( get_object_vars($this) ,function($value){
+            return $value!=null;
+        });
+        return $array;
+    }
 
-	/**
-	 * get properties as an associative array 
-	 * and trim null value
-	 * @see http://briancray.com/posts/remove-null-values-php-arrays
-	 */
-	function toArray(){
-	  $array = @array_filter( get_object_vars($this) ,function($value){
-		return $value!=null;
-	  });
-	  return $array;
-	}
+    function serialize()
+    {
+      return json_encode($this->toArray());
+    }
 
-	function serialize(){
-	  return json_encode($this->toArray());
-	}
-
-	function deszerialize($json){
-	  $datas = json_decode($json);
-	  $this->__construct($json);
-	}
+    function deszerialize($json)
+    {
+        $datas = json_decode($json);
+        $this->__construct($json);
+    }
 }
--- src/Esnuab/Libro/Model/Entity/Socio.php
+++ src/Esnuab/Libro/Model/Entity/Socio.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Esnuab\Libro\Model\Entity;
-class Socio extends Base {
+
+class Socio extends Base
+{
     protected $id;
     protected $nombre;
     protected $apellido;
@@ -12,76 +15,94 @@ class Socio extends Base {
     protected $expires_at;
     protected $mod_at;
 
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
-    public function setId($id){
+    public function setId($id)
+    {
         $this->id=$id;
         return $this;
     }
-    public function getNombre() {
+    public function getNombre()
+    {
         return $this->nombre;
     }
-    public function setNombre($nombre) {
+    public function setNombre($nombre)
+    {
         $this->nombre = ucwords($nombre);
         return $this;
     }
-    public function getApellido() {
+    public function getApellido()
+    {
         return $this->apellido;
     }
     public function setApellido($apellido) {
         $this->apellido = ucwords($apellido);
         return $this;
     }
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
-    public function setEmail($email) {
+    public function setEmail($email)
+    {
         $this->email = $email;
         return $this;
     }
-    public function getEsncard() {
+    public function getEsncard()
+    {
         return $this->esncard;
     }
     public function setEsncard($esncard) {
         $this->esncard = strtoupper($esncard);
         return $this;
     }
-    public function getPassport() {
+    public function getPassport()
+    {
         return $this->passport;
     }
-    public function setPassport($passport) {
+    public function setPassport($passport)
+    {
         $this->passport = strtoupper($passport);
         return $this;
     }
-    public function getPais() {
+    public function getPais()
+    {
         return $this->pais;
     }
-    public function setPais($pais) {
+    public function setPais($pais)
+    {
         $this->pais = $pais;
         return $this;
     }
-    public function getCreatedAt() {
+    public function getCreatedAt()
+    {
         return $this->created_at;
     }
-    public function setCreatedAt($created_at) {
+    public function setCreatedAt($created_at)
+    {
         $this->created_at = $created_at;
         return $this;
     }
-    public function getExpiresAt() {
+    public function getExpiresAt()
+    {
         return $this->expires_at;
     }
-    public function setExpiresAt() {
+    public function setExpiresAt()
+    {
         $time = new \DateTime();
         $time = \DateTime::createFromFormat('Y-m-d', $this->getCreatedAt());
         $time->modify("+1 year");
         $this->expires_at = $time->format('Y-m-d');
         return $this;
     }
-    public function getModAt() {
+    public function getModAt()
+    {
         return $this->mod_at;
     }
-    public function setModAt(){
+    public function setModAt()
+    {
         $time = new \DateTime(date('Y-m-d H:i:s', time()));
         $this->mod_at = $time->format('Y-m-d');
         return $this;
