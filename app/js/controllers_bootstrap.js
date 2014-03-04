4b74fe827e85e5cee0d9e443fb254f6744d8b99b
AlejandroHerr <alejandrohnc88@gmail.com>
2014-03-04 03:08:08 +0100

    Uses new "credenciales" service.

--- app/js/controllers_bootstrap.js
+++ app/js/controllers_bootstrap.js
@@ -1,46 +1,48 @@
-var DeleteModalInstanceCtrl = ['ApiCall', 'galletitas', '$scope',
-		'$modalInstance', 'socio',
-		function (ApiCall, galletitas, $scope, $modalInstance, socio) {
-			$scope.socio = socio;
-			$scope.alerts = [
-				{
-					type: 'warning',
-					msg: '¡Esta acción no se puede deshacer!'
-				},
-			  ];
-			$scope.addAlert = function (Type, Msg) {
-				$scope.alerts.push({
-					type: Type,
-					msg: Msg
-				});
-			};
-			$scope.closeAlert = function (index) {
-				$scope.alerts.splice(index, 1);
-			};
-			$scope.deletAlerts = function () {
-				$scope.alerts = '';
-			}
-			$scope.salir = function () {
-				$modalInstance.dismiss();
-			};
-			$scope.ok = function (str) {
-				if(str == 'ELIMINAR ' + $scope.socio.esncard) {
-					$scope.addAlert('success',
-						'Nuestros monos lo están borrando... Espera y no toques nada.');
-					var data = ApiCall.deleteSocio(galletitas.getXWSSE(), $scope.socio.id)
-						.then(function (d) {
-							if(d.status == 204) {
-								$modalInstance.close();
-							}
+'use strict';
+ar DeleteModalInstanceCtrl = ['ApiCall', 'credenciales', '$scope',
+	'$modalInstance', 'socio',
+	function (ApiCall, credenciales, $scope, $modalInstance, socio) {
+		$scope.socio = socio;
+		$scope.alerts = [{
+			type: 'warning',
+			msg: '¡Esta acción no se puede deshacer!'
+		}, ];
+		$scope.addAlert = function (Type, Msg) {
+			$scope.alerts.push({
+				type: Type,
+				msg: Msg
+			});
+		};
+		$scope.closeAlert = function (index) {
+			$scope.alerts.splice(index, 1);
+		};
+		$scope.deletAlerts = function () {
+			$scope.alerts = '';
+		}
+		$scope.salir = function () {
+			$modalInstance.dismiss();
+		};
+		$scope.ok = function (str) {
+			if(str == 'ELIMINAR ' + $scope.socio.esncard) {
+				$scope.addAlert('success',
+					'Nuestros monos lo están borrando... Espera y no toques nada.');
+				var data = ApiCall.deleteSocio(credenciales.getXWSSE(), $scope.socio.id)
+					.then(function (d) {
+							$modalInstance.close();
+						},
+						function (d) {
 							if(d.status == 401) {
-								$scope.addAlert('danger', '¡Tu sesion ha caducado..!');
-							}
-							if(d.status == 403) {
+								$scope.addAlert('danger', '¡Tus credenciales son incorrectas..!');
+							} else if(d.status == 403) {
 								$scope.addAlert('danger', '¡No tienes permiso para borrar socios!');
+							} else {
+								$scope.addAlert('danger', '¡Un error no esperado a sucedido!');
 							}
 						})
-				} else {
-					$scope.addAlert('danger', '¡Tienes que escribir la confirmación correcta!');
-				}
-			};
-	}];
+			} else {
+				$scope.addAlert('danger',
+					'¡Tienes que escribir la confirmación correcta!');
+			}
+		};
+	}
+];
