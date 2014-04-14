f6e37b1c925bbcdc9e28cfabf8ee2ef085208e8e
AlejandroHerr <alejandrohnc88@gmail.com>
2014-04-14 21:23:56 +0200

    Little functions for debugging with arrays

--- web/index.php
+++ web/index.php
@@ -1,6 +1,16 @@
 <?php
 define('ROOT',dirname(dirname(__FILE__)));
 date_default_timezone_set('Europe/Madrid');
+function printArray($arrayname=array()){
+	echo "<pre>";
+	print_r($arrayname);
+	echo '</pre>';
+}
+function dumpArray($arrayname=array()){
+	echo "<pre>";
+	var_dump($arrayname);
+	echo '</pre>';
+}
 require_once ROOT.'/src/config.php';
 
 
