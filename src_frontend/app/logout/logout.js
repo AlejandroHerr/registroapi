ac5ed76602b20eae1b995978c57b9dc8a76c3622
AlejandroHerr <alejandrohnc88@gmail.com>
2014-04-19 05:55:08 +0200

    Angularjs files moved to new dir

---
+++ src_frontend/app/logout/logout.js
@@ -0,0 +1,8 @@
+angular.module('libroApp.logout', [])
+
+.controller('LogoutCtrl', ['credenciales', '$scope', '$location',
+	function (credenciales, $scope, $location) {
+		credenciales.logOut();
+		$location.url('/app/login');
+	}
+]);
