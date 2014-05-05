4d4077a9bc72e3068ad450983476918b12605809
AlejandroHerr <alejandrohnc88@gmail.com>
2014-05-05 02:08:19 +0200

    Apicontroller becomes SocioController

--- src/Esnuab/Libro/routes.php
+++ src/Esnuab/Libro/routes.php
@@ -1,4 +1,4 @@
 <?php
 
 $app->mount('/admin', new Esnuab\Libro\Controller\AdminController($app['user_manager'],$app['monolog.transaction']));
-$app->mount('/', new Esnuab\Libro\Controller\ApiController($app['socio_manager'],$app['monolog.transaction']));
+$app->mount('/', new Esnuab\Libro\Controller\SocioController($app['socio_manager'],$app['monolog.transaction']));
