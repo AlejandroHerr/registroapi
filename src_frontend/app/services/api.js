c6db6770faa86146e3cb1a770000b1a2c9231384
AlejandroHerr <alejandrohnc88@gmail.com>
2014-05-21 17:00:03 +0200

    Low- and high-level function.

--- src_frontend/app/services/api.js
+++ src_frontend/app/services/api.js
@@ -1,25 +1,61 @@
 angular.module('libroApp.api', [])
-    .factory('ApiCall', ['$http',
-        function($http) {
+    .factory('ApiCall', ['$http', '$modal', '$state', 'loader',
+        function ($http, $modal, $state, loader) {
             $http.defaults.useXDomain = true;
             delete $http.defaults.headers.common['X-Requested-With'];
             var domain = 'remoteBackendURI';
-            var ApiCall = {
-                makeCall: function(passwordDigest, method, path, data) {
-                    $http.defaults.headers.common = {
-                        'X-WSSE': passwordDigest
-                    };
-                    var promise = $http({
-                            method: method,
-                            url: domain + path,
-                            data: data
-                        })
-                        .then(function(response) {
-                            return response;
-                        });
-                    return promise;
-                }
+            var makeCall = function (passwordDigest, method, path, data) {
+                loader.setLoading();
+                $http.defaults.headers.common = {
+                    'X-WSSE': passwordDigest
+                };
+                var promise = $http({
+                        method: method,
+                        url: domain + path,
+                        data: data
+                    })
+                    .then(function (response) {
+                        return response;
+                    });
+                promise['finally'](function () {
+                    loader.unsetLoading();
+                });
+                return promise;
             };
-            return ApiCall;
+            var apiCall = function (passwordDigest, method, path, data, successCb) {
+                makeCall(passwordDigest, method, path, data)
+                    .then(function (d) {
+                        successCb(d);
+                    }, function (d) {
+                        var modalInstance = $modal.open({
+                            templateUrl: 'modal/40x.tpl.html',
+                            controller: 'ErrorModalInstanceCtrl',
+                            resolve: {
+                                error: function () {
+                                    return d;
+                                }
+                            }
+                        });
+                        modalInstance.result.then(function () {}, function () {
+                            if (d.status == 401) {
+                                $state.go('logout', {}, {
+                                    location: true
+                                });
+                            }
+                            else {
+                                $state.go('logged', {}, {
+                                    location: true
+                                });
+                            }
+                        });
+                    }, function (d) {
+
+                    });
+
+            };
+            return {
+                apiCall: apiCall,
+                makeCall: makeCall
+            }
         }
     ]);
