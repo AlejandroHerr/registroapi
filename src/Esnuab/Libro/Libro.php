0434ea7e0ede6be99d3615e92cb4da329731ebf4
AlejandroHerr <alejandrohnc88@gmail.com>
2014-06-15 18:58:12 +0200

    Updated dependencies

--- src/Esnuab/Libro/Libro.php
+++ src/Esnuab/Libro/Libro.php
@@ -1,20 +1,26 @@
 <?php
 
+use AlejandroHerr\ApiApplication\Application;
+use AlejandroHerr\AuditLog\Formatter\AuditFormatter;
+use AlejandroHerr\AuditLog\Handler\DbalHandler;
+use AlejandroHerr\AuditLog\Processor\RequestProcessor;
+use AlejandroHerr\AuditLog\Processor\UserProcessor;
+use Esnuab\Libro\Model\Manager\SocioManager;
+use Esnuab\Libro\Model\Manager\UserManager;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\MonologServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
-$app = new Silex\Application();
+
+$app = new Application();
 $app['debug'] = true;
+
 //DB
 $app['db.config'] = require_once ROOT . '/config/db.php';
-$app->register(new Silex\Provider\DoctrineServiceProvider(),$app['db.config']);
-$app['socio_manager'] = $app->share(function ($app) {
-    return new \Esnuab\Libro\Model\Manager\SocioManager($app['db']);
-});
-$app['user_manager'] = $app->share(function ($app) {
-    return new \Esnuab\Libro\Model\Manager\UserManager($app['db']);
-});
+$app->register(new DoctrineServiceProvider(),$app['db.config']);
+
 //MONOLOG
-$app->register(new Silex\Provider\MonologServiceProvider());
+$app->register(new MonologServiceProvider());
 $app['monolog.logfile']=function () {
     $date = \DateTime::createFromFormat('U',time());
     $file = ROOT.'/var/logs/app_'.$date->format('Y-m-d').'.log';
@@ -29,15 +35,25 @@ $app['monolog.factory'] = $app->protect(function ($name) use ($app) {
 foreach (array('access','transaction') as $channel) {
     $app['monolog.'.$channel] = $app->share(function () use ($app,$channel) {
             $log = new $app['monolog.logger.class']($channel);
-            $handler = new Esnuab\AuditLog\Handler\DbalHandler($app['db']);
-            $handler->setFormatter(new Esnuab\AuditLog\Formatter\AuditFormatter());
-            $handler->pushProcessor(new Esnuab\AuditLog\Processor\RequestProcessor($app));
-            $handler->pushProcessor(new Esnuab\AuditLog\Processor\UserProcessor($app));
+            $handler = new DbalHandler($app['db']);
+            $handler->setFormatter(new AuditFormatter());
+            $handler->pushProcessor(new RequestProcessor($app));
+            $handler->pushProcessor(new UserProcessor($app));
             $log->pushHandler($handler);
 
             return $log;
         });
 }
+
+##################
+# MODEL MANAGERS #
+##################
+$app['socio_manager'] = $app->share(function ($app) {
+    return new SocioManager($app['db']);
+});
+$app['user_manager'] = $app->share(function ($app) {
+    return new UserManager($app['db']);
+});
 //SECURITY
 require_once 'security.php';
 foreach (array('user','admin','superadmin') as $role) {
