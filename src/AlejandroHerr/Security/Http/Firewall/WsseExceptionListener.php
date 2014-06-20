0a65442a87d713855cbb717d1139acd6d2512f5e
AlejandroHerr <alejandrohnc88@gmail.com>
2014-06-20 05:31:01 +0200

    Exception listener for the Wsse Firewall

---
+++ src/AlejandroHerr/Security/Http/Firewall/WsseExceptionListener.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AlejandroHerr\Security\Http\Firewall;
+
+use AlejandroHerr\Security\Core\Exception\WsseAuthenticationException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Security\Core\Authentication\AuthenticationTrustResolverInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\LogoutException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Security\Http\Firewall\ExceptionListener;
+
+class WsseExceptionListener extends ExceptionListener
+{
+
+    protected $logger;
+
+    public function __construct(SecurityContextInterface $context, AuthenticationTrustResolverInterface $trustResolver, HttpUtils $httpUtils, $providerKey, AuthenticationEntryPointInterface $authenticationEntryPoint = null, $errorPage = null, AccessDeniedHandlerInterface $accessDeniedHandler = null, LoggerInterface $logger = null)
+    {
+        parent::__construct($context, $trustResolver, $httpUtils, $providerKey, $authenticationEntryPoint, $errorPage, $accessDeniedHandler);
+        $this->logger = $logger;
+    }
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        do {
+            if ($exception instanceof WsseAuthenticationException) {
+                return $this->handleAuthenticationException($event, $exception);
+            } elseif ($exception instanceof AccessDeniedException) {
+                return $this->handleAccessDeniedException($event, $exception);
+            } elseif ($exception instanceof LogoutException) {
+                return $this->handleLogoutException($event, $exception);
+            }
+        } while (null !== $exception = $exception->getPrevious());
+    }
+
+    private function handleAuthenticationException(GetResponseForExceptionEvent $event, AuthenticationException $exception)
+    {
+        if (null !== $this->logger) {
+            $this->logger->info(sprintf('Wsse Authentication exception occurred (%i: %s)', $exception->getCode(),$exception->getMessage()));
+        }
+
+        try {
+            $message = array('message' => $exception->getMessage());
+            $response = new JsonResponse($message,401,array('Content-Type'=>'application/json'));
+            $event->setResponse($response);
+        } catch (\Exception $e) {
+            $event->setException($e);
+        }
+    }
+}
