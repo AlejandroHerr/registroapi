f16f81125f444786dbff8ad5260ea710d9207af5
AlejandroHerr <alejandrohnc88@gmail.com>
2014-06-20 05:36:36 +0200

    Unused use

--- src/AlejandroHerr/Stack/Cors.php
+++ src/AlejandroHerr/Stack/Cors.php
@@ -4,7 +4,6 @@ namespace AlejandroHerr\Stack;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Cors implements HttpKernelInterface
 {
