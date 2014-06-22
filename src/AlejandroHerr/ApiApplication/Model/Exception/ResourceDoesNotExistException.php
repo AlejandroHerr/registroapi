f9544152d73191862b4580bd249bf5ebc15c04fd
AlejandroHerr <alejandrohnc88@gmail.com>
2014-06-22 21:29:44 +0200

    Model Exception

---
+++ src/AlejandroHerr/ApiApplication/Model/Exception/ResourceDoesNotExistException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlejandroHerr\ApiApplication\Model\Exception;
+
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+class ResourceDoesNotExistException extends ResourceNotFoundException
+{
+    public function __construct($id)
+    {
+        parent::__construct(sprintf('El recurso %u no existe',$id),404);
+    }
+}
