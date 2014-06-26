c0d7e09b9a10ab9b75a06d3ba7b8e3e63227c921
AlejandroHerr <alejandrohnc88@gmail.com>
2014-06-26 19:18:53 +0200

    ApiApplication

--- src/config.php
+++ src/config.php
@@ -4,9 +4,10 @@ $loader = require ROOT . "/vendor/autoload.php";
 use Silex\Application;
 use Stack\LazyHttpKernel;
 use AlejandroHerr\Stack\Cors;
+use AlejandroHerr\ApiApplication\Application as App;
 use Symfony\Component\HttpFoundation\Request;
 
-$app=new Application();
+$app=new App();
 
 $api = new LazyHttpKernel(function () {
     return require 'Esnuab/Libro/Libro.php';
