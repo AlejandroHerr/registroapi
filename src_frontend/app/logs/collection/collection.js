3899f6b72f64128d54377667e6e094e5385243a0
AlejandroHerr <alejandrohnc88@gmail.com>
2014-07-03 02:08:27 +0200

    Removed unused dependencies

--- src_frontend/app/logs/collection/collection.js
+++ src_frontend/app/logs/collection/collection.js
@@ -1,14 +1,11 @@
 angular.module('libroApp.logs.collection', [])
-    .controller('LogsCollectionCtrl', ['ApiCaller', '$modal', 'queryParams', 'credentials', '$scope', '$state',
-        function (ApiCaller, $modal, queryParams, credentials, $scope, $state) {
-            
+    .controller('LogsCollectionCtrl', ['$scope', 'ApiCaller', 'credentials',
+        function ($scope, ApiCaller, credentials) {
             $scope.loadLogs = function () {
                 var path = '/api/logs/';
                 var data = ApiCaller.modalCall(credentials.getXWSSE(), 'GET', path, null, function (d) {
-                        $scope.logs = d.data.logs;
-                    }
-                );
-            };    
+                    $scope.logs = d.data.logs;
+                });
+            };
             $scope.loadLogs();
-        }
-    ]);
+        }]);
