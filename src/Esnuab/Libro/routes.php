722db31c6d8555c391ae283cc93021b40e4f7f6d
AlejandroHerr <alejandrohnc88@gmail.com>
2014-07-04 15:10:06 +0200

    Log routes added

--- src/Esnuab/Libro/routes.php
+++ src/Esnuab/Libro/routes.php
@@ -1,7 +1,9 @@
 <?php
 
 use Esnuab\Libro\Controller\AdminController;
+use Esnuab\Libro\Controller\LogController;
 use Esnuab\Libro\Controller\SocioController;
 
 $app->mount('/admin', new AdminController($app['user_manager'],$app['monolog.transaction']));
+$app->mount('/logs', new LogController($app['monolog.path']));
 $app->mount('/', new SocioController($app['socio_manager'],$app['monolog.transaction']));
