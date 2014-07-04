21af735115bcbb1373005d10ec5e06d40e3d8eb5
AlejandroHerr <alejandrohnc88@gmail.com>
2014-07-04 13:02:03 +0200

    Refactored

--- src_frontend/app/modal/modal.js
+++ src_frontend/app/modal/modal.js
@@ -60,32 +60,4 @@ angular.module('libroApp.modals', [])
                 $modalInstance.dismiss();
             };
         }
-    ])
-    .controller('RegistrarModalInstanceCtrl', ['ApiCaller', 'credentials', '$scope', '$modalInstance', 'item', 'url',
-        function (ApiCaller, credentials, $scope, $modalInstance, item, url) {
-            $scope.successFlag = false;
-            $scope.failFlag = false;
-            $scope.progress = "50";
-            $scope.status = "progress-bar-warning";
-            $scope.isCollapsed = false;
-            var path = url;
-            var data = ApiCaller.rawCall(credentials.getXWSSE(), 'POST', path, item)
-                .then(function (d) {
-                    $scope.successFlag = true;
-                    $scope.progress = "100";
-                    $scope.status = "progress-bar-success";
-                }, function (d) {
-                    $scope.failFlag = true;
-                    $scope.progress = "100";
-                    $scope.status = "progress-bar-danger";
-                    $scope.errors = d.data;
-                });
-            $scope.volver = function () {
-                $modalInstance.close();
-            }
-            $scope.salir = function () {
-                $modalInstance.dismiss();
-            };
-            //en caso afirmativo el tiene que ir a close, sino a dismiss
-        }
     ]);
--- src_frontend/app/socios/nuevo/nuevo.js
+++ src_frontend/app/socios/nuevo/nuevo.js
@@ -1,14 +1,14 @@
 angular.module('libroApp.socios.nuevo', [])
-    .controller('SociosNuevoCtrl', ['loader', 'ApiCaller', '$modal', 'credentials', '$http', '$scope', '$state', 'countries',
-        function (loader, ApiCaller, $modal, credentials, $http, $scope, $state, countries) {
+    .controller('SociosNuevoCtrl', ['$modal', '$scope', 'countries',
+        function ($modal, $scope, countries) {
             $scope.registrar = function () {
                 if (this.nuevoSocio.$invalid) {
                     return;
                 }
                 var socio = this.socio;
                 var modalInstance = $modal.open({
-                    templateUrl: 'modal/registrar.tpl.html',
-                    controller: 'RegistrarModalInstanceCtrl',
+                    templateUrl: 'socios/nuevo/modal.tpl.html',
+                    controller: 'SociosNuevoModalCtrl',
                     resolve: {
                         item: function () {
                             return socio;
@@ -46,5 +46,31 @@ angular.module('libroApp.socios.nuevo', [])
                     .toJSON()
                     .slice(0, 10)
             };
-        }
-    ]);
+        }])
+    .controller('SociosNuevoModalCtrl', ['$modalInstance', '$scope', 'ApiCaller', 'credentials', 'item', 'url',
+        function ($modalInstance, $scope, ApiCaller, credentials, item, url) {
+            $scope.successFlag = false;
+            $scope.failFlag = false;
+            $scope.progress = "50";
+            $scope.status = "progress-bar-warning";
+            $scope.isCollapsed = false;
+            var path = url;
+            var data = ApiCaller.rawCall(credentials.getXWSSE(), 'POST', path, item)
+                .then(function (d) {
+                    $scope.successFlag = true;
+                    $scope.progress = "100";
+                    $scope.status = "progress-bar-success";
+                }, function (d) {
+                    $scope.failFlag = true;
+                    $scope.progress = "100";
+                    $scope.status = "progress-bar-danger";
+                    $scope.errors = d.data;
+                });
+            $scope.volver = function () {
+                $modalInstance.close();
+            };
+            $scope.salir = function () {
+                $modalInstance.dismiss();
+            };
+            //en caso afirmativo el tiene que ir a close, sino a dismiss
+        }]);
