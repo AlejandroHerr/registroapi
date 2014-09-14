679301caa74487835ad6e9779efe5a1df95a392e
AlejandroHerr <alejandrohnc88@gmail.com>
2014-09-14 17:45:13 +0200

    Controllers as ControllerServiceProvider

--- src/Esnuab/Libro/routes.php
+++ src/Esnuab/Libro/routes.php
@@ -1,9 +1,26 @@
 <?php
 
-use Esnuab\Libro\Controller\UserController;
-use Esnuab\Libro\Controller\LogController;
-use Esnuab\Libro\Controller\SocioController;
+use Esnuab\Libro\Controller\LogControllerProvider;
+use Esnuab\Libro\Controller\SocioControllerProvider;
+use Esnuab\Libro\Controller\UserControllerProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
 
-$app->mount('/user', new UserController($app['user_manager'],null,$app['task_scheduler']));
-$app->mount('/logs', new LogController($app['monolog.path']));
-$app->mount('/socio', new SocioController($app['socio_manager'],null,$app['task_scheduler']));
+$app->register(new ServiceControllerServiceProvider());
+
+$logControllerProvider = new LogControllerProvider;
+$app->register($logControllerProvider);
+$app->mount('/log', $logControllerProvider);
+
+$socioControllerProvider = new SocioControllerProvider;
+$app->register($socioControllerProvider, array(
+    'libro.socio_controller.entity' => 'Esnuab\Libro\Model\Entity\Socio',
+    'libro.socio_controller.form' => 'Esnuab\Libro\Form\SocioForm'
+));
+$app->mount('/socio', $socioControllerProvider);
+
+$userControllerProvider = new UserControllerProvider;
+$app->register($userControllerProvider, array(
+    'libro.user_controller.entity' => 'Esnuab\Libro\Model\Entity\User',
+    'libro.user_controller.form' => 'Esnuab\Libro\Form\UserForm'
+));
+$app->mount('/user', $userControllerProvider);
